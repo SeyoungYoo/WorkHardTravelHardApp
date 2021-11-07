79c17245bd872c7e512f294e35484e70e94ac807
SeyoungYoo <yoogus12@gmail.com>
2021-11-07 18:01:10 +0900

    #3.3 To Dos Learning about onSubmitEditing and Object.assign to make To Dos

--- App.js
+++ App.js
@@ -6,9 +6,23 @@ import { theme } from './colors';
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") {
+      return ;
+    }
+    // save To Do
+    const newToDos = Object.assign(
+      {},
+      toDos, 
+      {[Date.now()]: {text, work: working}}
+      );
+    setToDos(newToDos);
+    setText("");
+  }
 
   return (
     <View style={styles.container}>
@@ -22,7 +36,9 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput 
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
+        returnKeyType='done'
         value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"} style={styles.input}/>
     </View>
