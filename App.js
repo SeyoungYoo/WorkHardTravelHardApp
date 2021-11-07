3fa49198ef18782bd4bdae3090bb7443e5122a6f
SeyoungYoo <yoogus12@gmail.com>
2021-11-07 15:06:10 +0900

    #3.2 TextInput Learning about how to input the text by TextInput and its props

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, TextInput } from 'react-native';
 import { theme } from './colors';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? 'white' : theme.gray }}>Work</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? 'white' : theme.gray }}>Travel</Text>
         </TouchableOpacity>
       </View>
+      <TextInput 
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"} style={styles.input}/>
     </View>
   );
 }
@@ -33,6 +43,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 44,
     fontWeight: '600',
-    color: 'white',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    marginTop: 25,
+    fontSize: 15,
   }
 });
