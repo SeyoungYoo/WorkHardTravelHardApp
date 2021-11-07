c86705fff4b00b15155b5bfa98061df87c8835c7
SeyoungYoo <yoogus12@gmail.com>
2021-11-07 19:32:39 +0900

    #3.5 Persist add AsyncStorage to save/load toDos

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, TextInput, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { theme } from './colors';
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
-  const addToDo = () => {
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
+  }
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  }
+
+  const addToDo = async () => {
     if (text === "") {
       return ;
     }
     // save To Do
     const newToDos = {
       ...toDos,
-      [Date.now()]: {text, work: working},
+      [Date.now()]: {text, working},
     };
     setToDos(newToDos);
+    await saveToDos(newToDos);
     setText("");
   }
 
@@ -43,9 +59,10 @@ export default function App() {
       <ScrollView>
         {
           Object.keys(toDos).map( key =>
+            toDos[key].working === working ? (
             <View style={styles.toDo} key={key}>
               <Text style={styles.toDoText}>{toDos[key].text}</Text>
-            </View>
+            </View> ) : null
           )
         }
       </ScrollView>
